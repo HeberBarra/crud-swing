70252e46844094c39eb1f3400dbc86c2c52ab970
Heber F Barra <heberferreirabarra@gmail.com>
2024-11-13 17:32:34 -0300

    Adição de depedências e criação de lógica básica do CRUD

---
+++ src/main/java/org/ifpr/crudta/usuario/Usuario.java
@@ -0,0 +1,38 @@
+package org.ifpr.crudta.usuario;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    @Column(name = "sexo")
+    @Enumerated(EnumType.STRING)
+    private SexoPessoa sexoPessoa;
+
+}
