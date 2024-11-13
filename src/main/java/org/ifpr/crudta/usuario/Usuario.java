70130c566689896cdb435d42f4acbce2c9b4e93b
Heber F Barra <heberferreirabarra@gmail.com>
2024-11-13 17:35:55 -0300

    Criação de getters e setters

--- src/main/java/org/ifpr/crudta/usuario/Usuario.java
+++ src/main/java/org/ifpr/crudta/usuario/Usuario.java
@@ -35,4 +35,51 @@ public class Usuario {
     @Enumerated(EnumType.STRING)
     private SexoPessoa sexoPessoa;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public SexoPessoa getSexoPessoa() {
+        return sexoPessoa;
+    }
+
+    public void setSexoPessoa(SexoPessoa sexoPessoa) {
+        this.sexoPessoa = sexoPessoa;
+    }
 }
